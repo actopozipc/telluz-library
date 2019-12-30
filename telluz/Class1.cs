c634e44494325adc1cb233801dbe266fb5ab287b
actopozipc <48481041+actopozipc@users.noreply.github.com>
2019-12-30 04:06:35 +0100

    library implementiert

---
+++ telluz/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telluz
+{
+    [Serializable]
+    public class Request
+    {
+       public int coa_id;
+        public int cat_id;
+        public int from;
+        public int to;
+
+    }
+    public class Respond
+    {
+        public float value;
+        public bool berechnet;
+        public float year;
+
+    }
+}
