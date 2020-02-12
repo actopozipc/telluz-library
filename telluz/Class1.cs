1d0a0d9eb62caaa82c2e875a8d681f55757db34e
actopozipc <48481041+actopozipc@users.noreply.github.com>
2020-02-12 04:36:36 +0100

    Value-Pairs as list object
    Error-Message wird jetzt nicht für jeden Wert übertragen

--- telluz/Class1.cs
+++ telluz/Class1.cs
@@ -9,16 +9,16 @@ namespace telluz
     [Serializable]
     public class Request
     {
-       public int coa_id;
+        public int coa_id;
         public int cat_id;
         public int from;
         public int to;
         public string key;
-        public type  typ;
-     
+        public type typ;
+
         public Request(int coa_id, int cat_id, int from, int to)
         {
-            
+
             this.coa_id = coa_id;
             this.cat_id = cat_id;
             this.from = from;
@@ -40,20 +40,20 @@ namespace telluz
         }
 
     }
-     public class Error
+    public class Error
     {
-       public string errorMessage;
-      public  bool hasErrors;
+        public string errorMessage;
+        public bool hasErrors;
         public Error(Request request)
         {
             hasErrors = false;
             errorMessage = "";
             if (request.cat_id < 0 || request.cat_id > 77)
             {
-                errorMessage= "Non-existing category";
+                errorMessage = "Non-existing category";
                 hasErrors = true;
             }
-            if (request.coa_id <0 || request.coa_id > 264 )
+            if (request.coa_id < 0 || request.coa_id > 264)
             {
                 errorMessage = "Non-existing country or area";
                 hasErrors = true;
@@ -63,11 +63,11 @@ namespace telluz
                 errorMessage = "Year too early";
                 hasErrors = true;
             }
-            
+
         }
-       
+
     }
-    
+
     public enum type
     {
         Daten,
@@ -76,21 +76,31 @@ namespace telluz
     [Serializable]
     public class Response
     {
-        public float value;
-        public bool berechnet;
-        public float year;
-        public string error_message;
-
+        public List<ValuePair> valuePair;
+        public string errorMessage;
+        public double colorVal { get; set; }
         public Response(Error error)
         {
-            this.value = -210;
-            this.berechnet = false;
-            this.year = -210;
-            this.error_message = error.errorMessage;
+            this.valuePair = new List<ValuePair>() { new ValuePair(false, -210, -210) };
+            this.errorMessage = error.errorMessage;
         }
         public Response()
         {
 
+        }
+        [Serializable]
+        public class ValuePair
+        {
+            public bool berechnet;
+            public float year;
+            public float value;
+
+            public ValuePair(bool calculated, float year, float value)
+            {
+                berechnet = calculated;
+                this.year = year;
+                this.value = value;
+            }
         }
     }
 }
